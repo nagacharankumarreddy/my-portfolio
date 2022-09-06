133e55bbcd11ce711fde6d54d8d9bc6525b709f2
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2022-09-06 19:58:29 +0530

    All files added

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,20 @@
+import { Col } from "react-bootstrap";
+
+export const ProjectCard = ({ title, description, imgUrl,linkUrl }) => {
+  return (
+    
+    <Col size={12} sm={6} md={4}>
+      
+      <a href="https://nagacharankumarreddy.github.io/interest-calculator/">
+      <div className="proj-imgbx">
+        <img src={imgUrl} />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+          <span>{description}</span>
+        </div>
+      </div>
+      </a>
+
+    </Col>
+  )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,77 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import tictactoe from "../assets/img/tictactoe.png";
+import markdown from "../assets/img/markdown.png";
+import calculator from "../assets/img/calculators.png"
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Tic-Tac-Toe",
+      description: "Two-player Game",
+      imgUrl: tictactoe,
+    },
+    {
+      title: "Mark-Down Editor",
+      description: "Text Formatting",
+      imgUrl: markdown,
+    },
+    {
+      title: "Calculator ",
+      description: "Tool for Quick calculation",
+      imgUrl: calculator,
+      linkUrl:"https://nagacharankumarreddy.github.io/interest-calculator/"
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>description be filled</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
