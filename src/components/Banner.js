4f686f66716513ee3ed8de8b92ec8e5c0bea5aed
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2022-11-24 13:49:03 +0530

    pic added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/bwcharan.png";
+import headerImg from "../assets/img/charanbw.png";
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
