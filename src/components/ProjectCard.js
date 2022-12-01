351a5da91a71e0c35fc7e1b4479871e3898b44ab
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2022-12-01 23:59:53 +0530

    fade animation added

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ import "./projectcard.css"
 export const ProjectCard = ({ title, description, imgUrl,linkUrl }) => {
   return (
     
-    <Col size={12} sm={6} md={4} data-aos="fade-up" 
+    <Col size={12} sm={6} md={4} data-aos="flip-left" 
     data-aos-duration="1500" >
       
       <a href={linkUrl} target="_blank">
