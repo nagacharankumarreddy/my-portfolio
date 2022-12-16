ed4fcc6d61ad3c6b59844ad4f1fc759195b98af7
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2022-12-16 23:58:22 +0530

    git link added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,14 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import navIcon1 from '../assets/img/linkedin.svg';
-import navIcon2 from '../assets/img/gmail.svg';
-import navIcon3 from '../assets/img/phone.svg';
-import { HashLink } from 'react-router-hash-link';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import navIcon1 from "../assets/img/linkedin.svg";
+import navIcon2 from "../assets/img/gmail.svg";
+import navIcon3 from "../assets/img/phone.svg";
+import navIcon4 from "../assets/img/github.svg";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const NavBar = () => {
-
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -20,16 +18,16 @@ export const NavBar = () => {
       } else {
         setScrolled(false);
       }
-    }
+    };
 
     window.addEventListener("scroll", onScroll);
 
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
-  }
+  };
 
   return (
     <Router>
@@ -40,23 +38,69 @@ export const NavBar = () => {
           </Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-              <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
+              <Nav.Link
+                href="#home"
+                className={
+                  activeLink === "home" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("home")}
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#skills"
+                className={
+                  activeLink === "skills" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("skills")}
+              >
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href="#project"
+                className={
+                  activeLink === "project"
+                    ? "active navbar-link"
+                    : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("project")}
+              >
+                Projects
+              </Nav.Link>
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/naga-charan-kumar-reddy-11378616a/"><img src={navIcon1} alt="linkedin" /></a>
-                <a href="mailto:nagacharankumarreddy@gmail.com"><img src={navIcon2} alt="mail" /></a>
-                <a href="tel:8309340949"><img src={navIcon3} alt="call" /></a>
+                <a
+                  href="https://www.linkedin.com/in/naga-charan-kumar-reddy-11378616a/"
+                  target={"blank"}
+                >
+                  <img src={navIcon1} alt="linkedin" />
+                </a>
+                <a
+                  href="mailto:nagacharankumarreddy@gmail.com"
+                  target={"blank"}
+                >
+                  <img src={navIcon2} alt="mail" />
+                </a>
+                <a href="tel:8309340949" target={"blank"}>
+                  <img src={navIcon3} alt="call" />
+                </a>
+                <a
+                  href="https://github.com/nagacharankumarreddy/"
+                  target={"blank"}
+                >
+                  <img src={navIcon4} alt="github" />
+                </a>
               </div>
-              <HashLink to='#contact'>
-              <button><span>Contact</span></button>
+              <HashLink to="#contact">
+                <button>
+                  <span>Contact</span>
+                </button>
               </HashLink>
             </span>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </Router>
-  )
-}
+  );
+};
